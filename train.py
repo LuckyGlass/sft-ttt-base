ef51641c43b9a8a2176159c572b6f9b620bb5130
lumen <lumen2004.scholar@gmail.com>
2024-10-19 16:59:46 +0800

    feat: enable 4bit + lora.

--- train.py
+++ train.py
@@ -21,7 +21,8 @@ import torch
 import transformers
 import utils
 from torch.utils.data import Dataset
-from transformers import Trainer
+from transformers import Trainer, BitsAndBytesConfig
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 
 IGNORE_INDEX = -100
 DEFAULT_PAD_TOKEN = "[PAD]"
@@ -50,6 +51,9 @@ class ModelArguments:
 @dataclass
 class DataArguments:
     data_path: str = field(default=None, metadata={"help": "Path to the training data."})
+    len_segment: int = field(default=2)
+    len_offset: int = field(default=1)
+    block_size: int = field(default=1024)
 
 
 @dataclass
@@ -60,6 +64,10 @@ class TrainingArguments(transformers.TrainingArguments):
         default=512,
         metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
     )
+    enable_lora: bool = field(default=False)
+    lora_rank: Optional[int] = field(default=None)
+    load_in_4bit: bool = field(default=False)
+    load_in_8bit: bool = field(default=False)
 
 
 def smart_tokenizer_and_embedding_resize(
@@ -127,7 +135,7 @@ def preprocess(
 class SupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer):
+    def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer, len_segment: int, len_offset: int, block_size: int, model_max_length: int):
         super(SupervisedDataset, self).__init__()
         logging.warning("Loading data...")
         list_data_dict = utils.jload(data_path)
@@ -136,22 +144,45 @@ class SupervisedDataset(Dataset):
         # Formatting for Bamboo-style timeline-reorder and tokenizing
         self.input_ids = []
         self.labels = []
+        len_segment = len_segment * block_size
+        len_offset = len_offset * block_size
         for example in list_data_dict:
-            prompts = [
-                "Please summary the event described in the following piece of texts in one sentence.",
-                f"The piece of texts is: {example['input']}",
-                "Please summary the event described in the piece of texts in one sentence. Please do not output anything else."
-            ]
-            messages = [
-                {'role': 'system', 'content': "You are a helpful assistant."},
-                {'role': 'user', 'content': '\n'.join(prompts)},
-                {'role': 'assistant', 'content': ' < '.join(f'[{i}]' for i in example['answers'])}
-            ]
-            input_ids = tokenizer.apply_chat_template(messages[:-1], return_tensors='pt', add_generation_prompt=True, return_dict=True)['input_ids'].flatten()
-            labels = tokenizer.apply_chat_template(messages, return_tensors='pt', add_generation_prompt=False, return_dict=True)['input_ids'].flatten()
-            labels[:input_ids.shape[-1]] = IGNORE_INDEX
-            self.input_ids.append(input_ids)
-            self.labels.append(labels)
+            context_ids = tokenizer(example['input'] + tokenizer.bos_token, add_special_tokens=False, return_tensors='pt').input_ids.flatten()
+            self.input_ids += [context_ids[s:s+len_segment] for s in range(0, context_ids.shape[-1], len_offset)]
+            self.labels += [context_ids[s:s+len_segment] for s in range(0, context_ids.shape[-1], len_offset)]
+            for qa in example['qa_pairs']:
+                prompts = [
+                    "Please sort the given events in the order of their appearance in the following long texts, from first to last.",
+                    example['input'],
+                    "Please sort the given events in the order of their appearance in the long texts, from first to last. The given events are:",
+                ]
+                prompts += [f"[{i + 1}]: {summary}" for i, summary in enumerate(qa['summaries'])]
+                prompts += ["For example, a valid answer is [2] < [3] < [1] < [4] < [5]."]
+                messages = [
+                    {'role': 'system', 'content': "You are a helpful assistant."},
+                    {'role': 'user', 'content': '\n'.join(prompts)},
+                    {'role': 'assistant', 'content': ' < '.join(f'[{i}]' for i in qa['answers'])}
+                ]
+                input_length = tokenizer.apply_chat_template(messages[:-1], return_tensors='pt', add_generation_prompt=True, return_dict=True)['input_ids'].shape[-1]
+                input_ids = tokenizer.apply_chat_template(messages, return_tensors='pt', add_generation_prompt=False, return_dict=True)['input_ids'].flatten()
+                output_length = input_ids.shape[-1] - input_length
+                if input_ids.shape[-1] > model_max_length:
+                    input_ids = torch.concat((input_ids[:model_max_length//2], input_ids[-model_max_length//2:]), dim=-1)
+                    input_length = len(input_ids) - output_length
+                labels = input_ids.clone()
+                labels[:input_length] = IGNORE_INDEX
+
+                assert input_ids.shape[-1] <= model_max_length
+                self.input_ids.append(input_ids)
+                self.labels.append(labels)
+        for i in range(len(self.input_ids)):
+            len_pad = model_max_length - self.input_ids[i].shape[-1]
+            assert len_pad >= 0
+            if len_pad > 0:
+                pad = torch.LongTensor([tokenizer.pad_token_id] * len_pad)
+                self.input_ids[i] = torch.concat((self.input_ids[i], pad), dim=-1)
+                pad = torch.LongTensor([IGNORE_INDEX] * len_pad)
+                self.labels[i] = torch.concat((self.labels[i], pad), dim=-1)
 
     def __len__(self):
         return len(self.input_ids)
@@ -179,9 +210,9 @@ class DataCollatorForSupervisedDataset(object):
         )
 
 
-def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args) -> Dict:
+def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args: DataArguments, model_max_length: int) -> Dict:
     """Make dataset and collator for supervised fine-tuning."""
-    train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path)
+    train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path, len_segment=data_args.len_segment, len_offset=data_args.len_offset, block_size=data_args.block_size, model_max_length=model_max_length)
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
     return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
 
@@ -190,10 +221,43 @@ def train():
     parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_args.model_name_or_path,
-        cache_dir=training_args.cache_dir,
-    )
+    if training_args.load_in_4bit:
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            cache_dir=training_args.cache_dir,
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_compute_dtype=torch.bfloat16,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type='nf4'
+            )
+        )
+    elif training_args.load_in_8bit:
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            cache_dir=training_args.cache_dir,
+            quantization_config=BitsAndBytesConfig(
+                load_in_8bit=True,
+            )
+        )
+    else:
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            cache_dir=training_args.cache_dir,
+        )
+    if training_args.enable_lora:
+        lora_config = LoraConfig(
+            r=training_args.lora_rank,
+            lora_alpha=2 * training_args.lora_rank,
+            target_modules="all-linear",
+            lora_dropout=0.05,
+            bias="none",
+            task_type="CAUSAL_LM"
+        )
+        model = get_peft_model(model, lora_config)
+        model = prepare_model_for_kbit_training(model)
+        print(model)
+        model.print_trainable_parameters()
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
@@ -218,7 +282,7 @@ def train():
         model=model,
     )
 
-    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args, model_max_length=training_args.model_max_length)
     trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     trainer.train()
     trainer.save_state()
